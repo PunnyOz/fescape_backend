ec59a284d37dce77c727be190d4f080205f10846
Tawatwee Aiemarreerat <punnyoz1103@gmail.com>
2021-10-13 09:18:25 +0700

    debugging

--- app.py
+++ app.py
@@ -35,10 +35,13 @@ def token_required(f):
         if not token:
             return jsonMessage('a valid token is missing')
 
-        data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        current_user = User.query.filter_by(public_id=data['public_id'], password=data['password']).first()
-        if current_user is None or current_user.last_logout > data.exp:
-            return jsonMessage('token is invalid 1')
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            current_user = User.query.filter_by(public_id=data['public_id'], password=data['password']).first()
+            if current_user is None or current_user.last_logout > data['exp']:
+                return jsonMessage('token is invalid 1')
+        except:
+            return jsonMessage('token is invalid 2')
 
         return f(current_user, *args, **kwargs)
     return decorator
